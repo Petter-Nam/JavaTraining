cf407b6c5d38c4a275edea6770dafe26eb46e50c
Petter-Nam <150123411+Petter-Nam@users.noreply.github.com>
2024-02-13 03:12:08 +0900

    ds

--- 05_java_file/src/step5_02/file/FileEx03_문제.java
+++ 05_java_file/src/step5_02/file/FileEx03_문제.java
@@ -1,8 +1,5 @@
 package step5_02.file;
 
-import java.io.FileWriter;
-import java.io.IOException;
-
 //# 파일 저장하기 : 연습문제1
 
 
@@ -14,30 +11,6 @@ public class FileEx03_문제 {
 		
 		String[] names = {"김철수", "이만수", "이영희"};
 		int[] ages     = {	   20,     30,     40};
-		
-		String fileName = "fileTest01.txt";
-		
-		String data = "";
-		
-		for (int i = 0; i < names.length; i++) {
-			data += names[i];
-			data += "/";
-			data += ages[i];
-			if (i != names.length - 1) {
-				data += ",";
-			}
-		}
-		
-		FileWriter fw = null;
-		try {
-			fw = new FileWriter(fileName);
-			fw.write(data);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			try {fw.close();} catch (IOException e) {e.printStackTrace();}
-		}
-		
 		
 	}
 
