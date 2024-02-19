e68eaf39ba3e411d87872e49244fa89cfc7a2e8d
Petter-Nam <150123411+Petter-Nam@users.noreply.github.com>
2024-02-20 04:05:03 +0900

    ㅇㄴ

--- 05_java_file/src/step5_02/file/FileEx03_문제.java
+++ 05_java_file/src/step5_02/file/FileEx03_문제.java
@@ -1,4 +1,12 @@
 package step5_02.file;
+/*
+ * 2월 20일
+ *파일을 다루는건 자주 이용을 안하다보니 기억이 나질 않는다. 
+ * 많은양을 많이 소화하지 못하지만 그래도 계속 연습을 해두어야할것 같다.
+ * 
+ */
+import java.io.FileWriter;
+import java.io.IOException;
 
 //# 파일 저장하기 : 연습문제1
 
@@ -12,6 +20,26 @@ public class FileEx03_문제 {
 		String[] names = {"김철수", "이만수", "이영희"};
 		int[] ages     = {	   20,     30,     40};
 		
+		String fileName = "fileTestTraining.txt";
+		
+		String data = "";
+		
+		for (int i = 0; i < names.length; i++) {
+			data += names[i];
+			data += "/";
+			data += ages[i];
+			if (i != names.length - 1) {
+				data += ",";
+			}
+		}
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(fileName);
+			fw.write(data);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {try {fw.close();} catch (IOException e) {e.printStackTrace();}
+		}
 	}
 
 }
--- 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx08_연습3.java
+++ 07_java_object_array/src/step7_01/objectArray/ObjectArrayEx08_연습3.java
@@ -1,6 +1,15 @@
 package step7_01.objectArray;
-
+/*
+ * 2월 20일 
+ * 배열의 크기를 자동적으로 늘리는데에 3일이 걸렸다..... temp에 기존의 클래스 주소를 참조시켜서 배열을 유동적으로 늘리는게 이해가 안되서 계속 해보았다.
+ * 이해는 가지만 굳이 사용안하겠다는 느낌만 받고 시간만 허비했다고 생각한다....
+ * 어렵고 시간만 잡으면서 얻은건 있지만
+ * 괜히했다.
+ * 기억에도 오래 안남을거 같고 더 손대고싶진않다.
+ * 
+ */
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class Subject83 {
 	String name;
@@ -16,33 +25,102 @@ public class ObjectArrayEx08_연습3 {
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
-		Student83[] student = new Student83[1];
 		int studentCnt = 0;
-
+		int subjectCnt = 0;
+		Student83[] student = new Student83[studentCnt + 1];
 		while (true) {
-			
-			System.out.println("[1]학생 입력\n[2]과목 입력\n[3]점수입력\n[4]성적표 출력\n[5]종료");
-			System.out.print("입력 : ");
+			System.out.print("[1]학생 추가\n[2]과제 추가\n[3]점수추가\n[4]성적표 출력\n[5]종료\n입력 : ");
 			int choice = scan.nextInt();
-			
+			System.out.println();
+
 			if (choice == 1) {
-				System.out.println("이름 입력 : ");
-				String studentName = scan.next();	
-				
-				if (studentCnt >= student.length) {
+				System.out.print("이름 입력 : ");
+				String studentName = scan.next();
+
+				if (studentCnt == 0) {
 					student[studentCnt] = new Student83();
 					student[studentCnt].name = studentName;
 					studentCnt++;
+				} 
+				else {
+					Student83[] temp1 = new Student83[studentCnt + 1];
+
+					for (int i = 0; i < studentCnt; i++) {
+						temp1[i] = student[i];
+					}
+					temp1[studentCnt] = new Student83();
+					temp1[studentCnt].name = studentName;
+					student = temp1;
+					studentCnt++;
+					temp1 = null;
+				}
+				System.out.println();
+			}
+			else if (choice == 2) {
+				for (int i = 0; i < student.length; i++) {
+					System.out.print("[" + (i + 1) + "] " + student[i].name + " ");
+				}
+				
+				System.out.print("\n입력 : ");
+				int choiceStudent = scan.nextInt();
+				choiceStudent--;
+				
+				System.out.print("\n과목 입력 : ");
+				String subjectName = scan.next();
+				
+				student[choiceStudent].subject = new Subject83[subjectCnt + 1];
+				
+				if (subjectCnt == 0) {
+					student[choiceStudent].subject[subjectCnt] = new Subject83();
+					student[choiceStudent].subject[subjectCnt].name = subjectName;
+					subjectCnt++;
 				}
 				else {
-					Student83[] temp = new Student83[student.length];
-					temp[studentCnt] = new Student83();
-					temp[studentCnt].name = studentName;
-					studentCnt++;
-					student = temp;
+					Subject83[] temp = new Subject83[subjectCnt + 1];
+					
+					for (int i = 0; i < subjectCnt; i++) {
+						temp[i] = student[choiceStudent].subject[i];
+					}
+					temp[subjectCnt] = new Subject83();
+					temp[subjectCnt].name = subjectName;
+					student[choiceStudent].subject = temp;					
+					subjectCnt++;
+					temp = null;
 				}
-				continue;
 			}
+			else if (choice == 3) {
+				for (int i = 0; i < student.length; i++) {
+					System.out.print("[" + (i + 1) + "] " +student[i].name + " ");
+					for (int j = 0; j < student[i].subject.length; j++) {
+						System.out.print("[" + (j + 1) + "] " + student[i].subject[j].name + " ");
+					}
+					System.out.print("\n학생 넘버 입력");
+					int choiceStuNum = scan.nextInt();
+					choiceStuNum--;
+					
+					System.out.print("\n과목 넘버 입력 : ");
+					int subNum = scan.nextInt();
+					subNum--;
+					
+					System.out.print("\n점수 입력 : ");
+					int scoreInput = scan.nextInt();
+					
+					student[choiceStuNum].subject[subNum].score =scoreInput;
+				}
+			}
+			else if (choice == 4) {
+				for (int i = 0; i < student.length; i++) {
+					System.out.print("[" + (i + 1) + "] " +student[i].name + " ");
+					for (int j = 0; j < student[i].subject.length; j++) {
+						System.out.print("[" + (j + 1) + "] " + student[i].subject[j].name + " " + student[i].subject[j].score);
+					}
+			}
+				
 		}
+			else if (choice == 5) {
+				scan.close();
+			}
+
 	}
+}
 }
