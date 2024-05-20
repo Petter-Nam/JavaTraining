e0c70e403ffd370dfa327a850107c4902241fba4
Petter-Nam <150123411+Petter-Nam@users.noreply.github.com>
2024-05-20 13:45:18 +0900

    bb

---
+++ 19_spring_training6/src/main/java/com/application/training/board/dao/BoardDAO.java
@@ -0,0 +1,19 @@
+package com.application.training.board.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.application.training.board.dto.BoardDTO;
+
+@Mapper
+public interface BoardDAO {
+
+	public void createBoard(BoardDTO boardDTO);
+	public List<BoardDTO> getBoardList();
+	public BoardDTO getBoardDetail(long boardId);
+	public void updateReadCnt(long boardId);
+	public String getEncodedPasswd(long boardId);
+	public void updateBoard(BoardDTO boardDTO);
+	public void deleteBoard(long boardId);
+}
